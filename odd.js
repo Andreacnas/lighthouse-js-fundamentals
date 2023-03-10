3c68451a7ad3e9069fecc4a713e4587e69ce0f7f
Andrea <andreacnas@gmail.com>
2023-03-10 23:10:44 +0000

    Iterate over an array with for loop

--- odd.js
+++ odd.js
@@ -4,3 +4,10 @@ const isOdd  = function (num) {
 
 console.log("3 is odd: " + isOdd(3));
 console.log("8 is odd: " + isOdd(8));
+
+const stations = [
+  ['Big Bear Donair', 10, 'restaurant'],
+  ['Bright Lights Elementary', 50, 'school'],
+  ['Moose Mountain Community Centre', 45, 'community centre']
+];
+
